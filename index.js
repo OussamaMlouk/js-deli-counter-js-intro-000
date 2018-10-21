6f8dad61aac1afd80b84c6add8d218d3e6346f8b
Oussama Mlouk <o.mlouk@hotmail.co.uk>
2018-10-21 21:35:30 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,44 @@
+function takeANumber(array,name){
+  var position = array.length +1
+  array.push(name)
+  return `Welcome, ${name}. You are number ${position} in line.`
+}
+
+function nowServing(array){
+  if(array.length === 0){
+    return "There is nobody waiting to be served!"
+  }
+  else{
+  return `Currently serving ${array[0]}.`
+  array.shift()
+  }
+}
+
+function list(array){
+  for(i=0;i<array.length;i++){
+    text= `${i+1}.` + array[i] 
+  }
+  return text
+}
+
+function currentLine(array){
+  if(array.length === 0){
+    return "The line is currently empty."
+  }
+  else if (array.length === 1){
+    return `Currently serving ${array[0]}`
+  }
+  else{let i = 0
+    while(i<array.length){
+      var position = `${i+1}. ${array[i]}`
+      i++
+      var list = `${position}`
+    }
+    return `The line is currently: ${list}`
+  }}
+
+    //for(let i=0; i<array.length; i++){
+      //var list = `${i+1}. ${array[i]}`
+    //}
+    //return `The line is currently: ${list}`
+  //}}
